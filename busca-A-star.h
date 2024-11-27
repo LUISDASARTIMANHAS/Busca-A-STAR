9a223f6bf8aa099d277fdcc27e13c7e9c035bdf1
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2024-11-27 16:58:13 -0300

    Automated commit for busca-A-star.V0.0.1

---
+++ busca-A-star.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include "libs/fileSys.cpp"
+
+#define RASCUNHO_H // Define o identificador
+
+#define LINHAS 10
+#define COLUNAS 10
+
+typedef struct NoCaminho {
+    int x, y;
+    int custo_g; // Custo do início até o nó atual
+    int custo_h; // Heurística (distância até o objetivo)
+    int custo_f; // Custo total: custo_g + custo_h
+    struct NoCaminho* pai; // Nó pai para reconstrução do caminho
+} TNoCaminho;
+
+void ler_mapa(const char* nome_arquivo, int mapa[LINHAS][COLUNAS], int* inicio_x, int* inicio_y, int* objetivo_x, int* objetivo_y);
+void imprimir_mapa(int mapa[LINHAS][COLUNAS]);
+void busca_a_estrela(int mapa[LINHAS][COLUNAS], int inicio_x, int inicio_y, int objetivo_x, int objetivo_y);
+
+
+
